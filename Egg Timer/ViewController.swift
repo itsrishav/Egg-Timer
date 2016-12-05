490311c274dbce841bd56eadfeaebb241e3e2dc4
itsrishav <with.rishav@gmail.com>
2016-12-05 14:06:40 +0530

    Counter Added with layout changes

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -9,10 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = Timer()
+    var value = 300
 
+    @IBOutlet var counterValue: UILabel!
+   
+    @IBAction func pauseCounter(_ sender: AnyObject) {
+        timer.invalidate()
+    }
+    
+    @IBAction func playCounter(_ sender: AnyObject) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.counter), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func resetCounter(_ sender: AnyObject) {
+        value = 300
+        counterValue.text = "\(value)"
+    }
+
+    @IBAction func subTen(_ sender: AnyObject) {
+        value -= 10
+        counterValue.text = "\(value)"
+    }
+    
+    @IBAction func addTen(_ sender: AnyObject) {
+        value += 10
+        counterValue.text = "\(value)"
+    }
+    
+    func counter() {
+        counterValue.text = "\(value)"
+        value -= 1
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        counterValue.text = "\(value)"
     }
 
     override func didReceiveMemoryWarning() {
